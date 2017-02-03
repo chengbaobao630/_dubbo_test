34d153c436851bd91a3474bcd84c5efdb308deed
Cc <chengbaobao630@hotmail.com>
2017-02-03 12:17:38 +0800

    dubbo test

--- src/main/java/cc/home/DubboTestApplication.java
+++ src/main/java/cc/home/DubboTestApplication.java
@@ -1,15 +1,11 @@
 package cc.home;
 
-import com.alibaba.dubbo.config.annotation.Service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
-import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@ImportResource({"classpath:spring-dubbo.xml"})
-@ComponentScan(includeFilters = {@ComponentScan.Filter(type = FilterType.ANNOTATION,value = {Service.class})})
+//@ImportResource({"classpath:spring-dubbo.xml"})
+//@ComponentScan(includeFilters = {@ComponentScan.Filter(type = FilterType.ANNOTATION,value = {Service.class})})
 public class DubboTestApplication {
 
 	public static void main(String[] args) {
--- src/main/java/cc/home/dubbo/TestDubboImpl.java
+++ src/main/java/cc/home/dubbo/TestDubboImpl.java
@@ -1,10 +1,9 @@
 package cc.home.dubbo;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.alibaba.dubbo.config.annotation.Reference;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reomte.dubbo.ScheduleTest;
-import reomte.dubbo.UserService;
 
 /**
  * Created by cheng on 2017/1/12 0012.
@@ -13,16 +12,11 @@ import reomte.dubbo.UserService;
 @RequestMapping("dubbo")
 public class TestDubboImpl {
 
-    @Autowired
-    UserService userService;
+/*    @Autowired
+    UserService userService;*/
 
-    @Autowired
+    @Reference(version = "1.0.0")
     ScheduleTest scheduleTest;
-
-    @RequestMapping("users")
-    public String getUser() {
-        return userService.getUsers();
-    }
 
     @RequestMapping("schedule")
     public String scheduleTest(String name) {
